a407374577b918f3b8faa5b72007661eb01635a8
Kevin Huang <khuan028@ucr.edu>
2020-04-18 14:42:43 -0700

    Quick formatting fix for PlayerEntity script

--- Assets/Code/Runtime/Entities/PlayerEntity.cs
+++ Assets/Code/Runtime/Entities/PlayerEntity.cs
@@ -3,92 +3,104 @@ using System.Collections.Generic;
 using ExitGames.Client.Photon;
 using UnityEngine;
 
-public class PlayerEntity : EntityUnit {
+public class PlayerEntity : EntityUnit
+{
+	public static Dictionary<int, PlayerEntity> players;
 
-  public static Dictionary<int, PlayerEntity> players;
+	static PlayerEntity()
+	{
+		players = new Dictionary<int, PlayerEntity>();
+	}
 
-  static PlayerEntity(){
-    players = new Dictionary<int, PlayerEntity>();
-  }
+	[Header("Gameplay")]
+	public float speed = 5f;
 
-  [Header("Gameplay")]
-  public float speed = 5f;
+	[Header("Network")]
+	public float prevNetworkTime;
 
-  [Header("Network")]
-  public float prevNetworkTime;
+	public Vector3 prevPos;
+	public Vector3 nextPos;
+	public Vector3 dampVector;
 
-  public Vector3 prevPos;
-  public Vector3 nextPos;
-  public Vector3 dampVector;
+	public static PlayerEntity CreateEntity()
+	{
+		GameObject obj = Instantiate(GameInitializer.Instance.playerPrefab);
+		PlayerEntity entity = obj.GetComponent<PlayerEntity>();
+		return entity;
+	}
 
-  public static PlayerEntity CreateEntity(){
-    var obj = Instantiate(GameInitializer.Instance.playerPrefab);
-    var entity = obj.GetComponent<PlayerEntity>();
-    return entity;
-  }
+	public override void StartEntity()
+	{
+		base.StartEntity();
 
-  public override void StartEntity() {
-    base.StartEntity();
+		UnitManager.Local.players.Add(authorityID, this);
+	}
 
-    UnitManager.Local.players.Add(authorityID, this);
-  }
+	public override void UpdateEntity()
+	{
+		base.UpdateEntity();
 
-  public override void UpdateEntity() {
-    base.UpdateEntity();
+		if (isMine)
+		{
+			// movement
+			float hor = Input.GetAxisRaw("Horizontal");
+			float ver = Input.GetAxisRaw("Vertical");
 
-    if (isMine){
-      // movement
-      var hor = Input.GetAxisRaw("Horizontal");
-      var ver = Input.GetAxisRaw("Vertical");
+			transform.position += new Vector3(hor, 0f, ver) * Time.deltaTime * speed;
 
-      transform.position += new Vector3(hor, 0f, ver) * Time.deltaTime * speed;
+			// gun
+			if (Input.GetMouseButton(0))
+			{
+				BulletEntity bullet = BulletEntity.CreateEntity();
 
-      // gun
-      if (Input.GetMouseButton(0)){
-        var bullet = BulletEntity.CreateEntity();
-        
-        var playersp = Camera.main.WorldToScreenPoint(transform.position);
-        var camerasp = Input.mousePosition;
-        var dir = (camerasp - playersp).normalized;
+				Vector3 playersp = Camera.main.WorldToScreenPoint(transform.position);
+				Vector3 camerasp = Input.mousePosition;
+				Vector3 dir = (camerasp - playersp).normalized;
 
-        bullet.destination = dir * 10f;
+				bullet.destination = dir * 10f;
 
-        UnitManager.Local.Register(bullet);
+				UnitManager.Local.Register(bullet);
 
-      }
-    } else {
-      // intrepolation AND extrapolation
-      var lerpNetwork = Time.time - prevNetworkTime;
-      var extraPos = Vector3.Lerp(prevPos, nextPos, lerpNetwork / updateTimer);
-      
-      transform.position = Vector3.SmoothDamp(transform.position, extraPos, ref dampVector, Time.deltaTime * 2f);
-    }
-  }
+			}
+		}
+		else
+		{
+			// intrepolation AND extrapolation
+			float lerpNetwork = Time.time - prevNetworkTime;
+			Vector3 extraPos = Vector3.Lerp(prevPos, nextPos, lerpNetwork / updateTimer);
 
-  public override void DestroyEntity() {
-    base.DestroyEntity();
+			transform.position = Vector3.SmoothDamp(transform.position, extraPos, ref dampVector, Time.deltaTime * 2f);
+		}
+	}
 
-    UnitManager.Local.players.Remove(authorityID);
-  }
+	public override void DestroyEntity()
+	{
+		base.DestroyEntity();
 
-  public override void Serialize(ExitGames.Client.Photon.Hashtable h) {
-    base.Serialize(h);
+		UnitManager.Local.players.Remove(authorityID);
+	}
 
-    h.Add('p', transform.position);
-  }
+	public override void Serialize(ExitGames.Client.Photon.Hashtable h)
+	{
+		base.Serialize(h);
 
-  public override void Deserialize(ExitGames.Client.Photon.Hashtable h) {
-    base.Deserialize(h);
-    
-    prevNetworkTime = Time.time;
+		h.Add('p', transform.position);
+	}
 
-    // p
-    object val;
-    if (h.TryGetValue('p', out val)){
-      prevPos = transform.position;
-      nextPos = (Vector3)val;
-    }
-    
-  }
+	public override void Deserialize(ExitGames.Client.Photon.Hashtable h)
+	{
+		base.Deserialize(h);
+
+		prevNetworkTime = Time.time;
+
+		// p
+		object val;
+		if (h.TryGetValue('p', out val))
+		{
+			prevPos = transform.position;
+			nextPos = (Vector3)val;
+		}
+
+	}
 
 }
