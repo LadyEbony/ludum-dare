1e4a6d8ab22b3d3eb6195168aef34dee86a737ec
Kevin Huang <khuan028@ucr.edu>
2020-04-18 19:06:12 -0700

    Camera now points downward. Added Chris's level.

--- Assets/Code/Runtime/WorldParent.cs
+++ Assets/Code/Runtime/WorldParent.cs
@@ -1,21 +1,10 @@
-﻿/* WorldParent.cs
- * 
- * Scales all of its children vertically such that the children appear to have a consistent height no matter the camera angle
- * 
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldParent : MonoBehaviour
 {
     private static WorldParent _instance;
-
-    private Camera cam;
-    public bool dynamicScalingCorrection = false;
-    [Range(0, 89f)]
-    public float maxCorrectionAngle = 60f;
 
     public static WorldParent instance
     {
@@ -32,21 +21,4 @@ public class WorldParent : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        cam = Camera.main;
-    }
-
-    private void Update()
-    {
-        if(dynamicScalingCorrection && cam != null)
-        {
-            if(Mathf.Abs(cam.transform.eulerAngles.x) < maxCorrectionAngle)
-            {
-                Vector3 scale = transform.localScale;
-                scale.y = 1 / Mathf.Cos(cam.transform.eulerAngles.x * Mathf.Deg2Rad);
-                transform.localScale = scale;
-            }
-        }
-    }
 }
